29f688f0ba7021bc897f93801796f90d8921caba
juniordeveloper11 <89851068+juniordeveloper11@users.noreply.github.com>
2023-04-04 19:46:49 -0300

    Initial commit

---
+++ src/Calculo_Financeiro.java
@@ -0,0 +1,16 @@
+public class Calculo_Financeiro {
+    public static void main(String[] args) {
+
+        int [ ]     array  = new  int[ 6 ];
+
+        array  [ 0 ] = 2;
+        array  [1 ] = 4;
+        array  [ 2 ] = 10;
+        array  [3  ] = 5;
+        array [ 4 ]  = 15;
+        array [ 5 ]  =  3;
+
+        System.out.println( "qual  sera  o valor existente  no indice 3?\n0 elemento é :" + array[3]);
+
+    }
+}
